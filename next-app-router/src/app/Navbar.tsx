f546a824b21fb8ab46a53c1decdbd279ca9dad30
Ardhityo <aryaadi229@gmail.com>
2026-05-27 21:41:02 +0700

    4.Link & Navigation

--- next-app-router/src/app/Navbar.tsx
+++ next-app-router/src/app/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathName = usePathname();
   return (
     <nav className="flex justify-between px-28 py-5 items-center bg-gray-600 text-white">
       <Link href="/" className="text-2xl">
@@ -8,9 +10,21 @@ export default function Navbar() {
       </Link>
       <ul>
         <li className="flex gap-3 text-md items-center">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/about/profile">Profile</Link>
+          <Link href="/" className={pathName === "/" ? "text-gray-300" : ""}>
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className={pathName === "/about" ? "text-gray-300" : ""}
+          >
+            About
+          </Link>
+          <Link
+            href="/about/profile"
+            className={pathName === "/about/profile" ? "text-gray-300" : ""}
+          >
+            Profile
+          </Link>
         </li>
       </ul>
     </nav>
