f3a1254b9792a588c309a714aadc18737cb97c2a
Ardhityo <aryaadi229@gmail.com>
2026-05-28 14:18:50 +0700

    6.Data Fetching

--- next-app-router/src/app/api/products/route.ts
+++ next-app-router/src/app/api/products/route.ts
@@ -3,17 +3,24 @@ import { NextResponse } from "next/server";
 const products = [
   {
     id: 1,
-    name: "Product 1",
-    category: "Category 1",
-    price: 100,
-    image: "https://via.placeholder.com/150",
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+    category: "men's clothing",
+    rating: { rate: 3.9, count: 120 },
   },
   {
     id: 2,
-    name: "Product 2",
-    category: "Category 2",
-    price: 100,
-    image: "https://via.placeholder.com/150",
+    title: "Mens Casual Premium Slim Fit T-Shirts",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and …eat fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    category: "men's clothing",
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+    rating: { rate: 4.1, count: 120 },
   },
 ];
 
--- next-app-router/src/app/products/[[...slug]]/page.tsx
+++ next-app-router/src/app/products/[[...slug]]/page.tsx
@@ -1,18 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+
 type Params = {
   params: {
     slug: string[];
   };
 };
 
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+async function getProducts() {
+  // fetch data from api external
+  // const response = await fetch("https://fakestoreapi.com/products");
+
+  // fetch data from api internal
+  const response = await fetch("http://localhost:3000/api/products");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+}
+
 //[[...slug]] = berarti parameter /products/** bisa memiliki banyak parameter & tidak wajib memiliki parameter & bisa menjadi index jika tidak memiliki slug
 //[...slug] = berarti parameter /products/** bisa memiliki banyak parameter & tidak memiliki index
 //[slug] = berarti parameter /products/** hanya memiliki satu parameter
-export default function ProductPage({ params }: Params) {
+export default async function ProductPage({ params }: Params) {
   // http://localhost:3000/products/123/321
   const { slug } = params;
+
+  //products from api external
+  // const products = await getProducts();
+
+  // products from api internal
+  // destructuring object (data )+ rename variable (products).
+  const { data: products } = await getProducts();
+
   return (
     <>
-      <h1>{slug ? "Detail Product Page" : "Product Page"}</h1>
+      <h1 className="text-2xl font-semibold">
+        {slug ? "Detail Product Page" : "Product Page"}
+      </h1>
       <p>
         {slug && (
           <>
@@ -20,6 +60,119 @@ export default function ProductPage({ params }: Params) {
           </>
         )}
       </p>
+
+      <section className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 place-items-center">
+        {products.map((product: Product) => (
+          <div className="w-full max-w-sm bg-neutral-primary-soft p-6 border border-default rounded-base shadow-xs">
+            <Link href={`/products/${product.id}`}>
+              <Image
+                className="rounded-base mb-6"
+                src={product.image}
+                alt={product.title}
+                width={350}
+                height={350}
+              />
+            </Link>
+            <div>
+              <div className="flex items-center space-x-3 mb-6">
+                <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                  <svg
+                    className="w-5 h-5 text-fg-yellow"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                  </svg>
+                  <svg
+                    className="w-5 h-5 text-fg-yellow"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                  </svg>
+                  <svg
+                    className="w-5 h-5 text-fg-yellow"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                  </svg>
+                  <svg
+                    className="w-5 h-5 text-fg-yellow"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                  </svg>
+                  <svg
+                    className="w-5 h-5 text-fg-yellow"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                  </svg>
+                </div>
+                <span className="bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium px-1.5 py-0.5 rounded-sm">
+                  {product.rating.rate} out of 5
+                </span>
+              </div>
+              <a href="#">
+                <h5 className="text-xl text-heading font-semibold tracking-tight">
+                  {product.title}
+                </h5>
+              </a>
+              <div className="flex items-center justify-between mt-6">
+                <span className="text-3xl font-extrabold text-heading">
+                  ${product.price}
+                </span>
+                <button
+                  type="button"
+                  className="inline-flex items-center text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none"
+                >
+                  <svg
+                    className="w-4 h-4 me-1.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"
+                    />
+                  </svg>
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
     </>
   );
 }
