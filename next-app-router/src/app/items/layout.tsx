d06931ad5025b7410a9ac53c814d4e65d9b2a3d4
Ardhityo <aryaadi229@gmail.com>
2026-05-31 16:47:16 +0700

    19.SEO & Metadata

--- next-app-router/src/app/items/layout.tsx
+++ next-app-router/src/app/items/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My App",
+  description: "My Next app",
+};
+
 export default function ProductLayout({
   children,
   modal,
